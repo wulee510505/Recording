2f40f5994001db378e9f7b1f776c09be2f4e1eac
wulee510505 <wulee510505@gmail.com>
2018-07-21 15:08:46 +0800

    modfiy
    修改录音dialog样式

--- recordingibrary/src/main/java/com/wulee/recordingibrary/view/RecordVoiceButton.java
+++ recordingibrary/src/main/java/com/wulee/recordingibrary/view/RecordVoiceButton.java
@@ -1,16 +1,26 @@
 package com.wulee.recordingibrary.view;
 
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.wulee.recordingibrary.R;
 import com.wulee.recordingibrary.utils.VoiceManager;
+import com.yanzhenjie.permission.Action;
+import com.yanzhenjie.permission.AndPermission;
+
+import java.util.List;
 
 /**
  * 录音控件button
@@ -72,6 +82,20 @@ public class RecordVoiceButton extends AppCompatButton implements View.OnClickLi
         mIvPauseContinue= (ImageView) recordIndicator.findViewById(R.id.iv_continue_or_pause);
         mIvComplete= (ImageView) recordIndicator.findViewById(R.id.iv_complete);
         mIvCancel= (ImageView) recordIndicator.findViewById(R.id.iv_cancel);
+
+        //显示在屏幕底部
+        Window dialogWindow = recordIndicator.getWindow();
+        dialogWindow.setGravity( Gravity.BOTTOM);
+        dialogWindow.getDecorView().setPadding(0, 0, 0, 0); //消除边距
+
+        WindowManager wm = (WindowManager)getContext().getSystemService(Context.WINDOW_SERVICE);
+        int height = wm.getDefaultDisplay().getHeight();
+
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;   //设置宽度充满屏幕
+        lp.height = height/3;
+        dialogWindow.setAttributes(lp);
+
         recordIndicator.show();
         //暂停或继续
         mIvPauseContinue.setOnClickListener(new OnClickListener() {
@@ -106,43 +130,59 @@ public class RecordVoiceButton extends AppCompatButton implements View.OnClickLi
 
     @Override
     public void onClick(View view) {
-        startRecordDialog();
-        voiceManager.setVoiceRecordListener(new VoiceManager.VoiceRecordCallBack() {
-            @Override
-            public void recDoing(long time, String strTime) {
-                mRecordHintTv.setText(strTime);
-            }
+        AndPermission
+                .with(mContext)
+                .runtime()
+                .permission(Manifest.permission.RECORD_AUDIO)
+                .onGranted(new Action<List<String>>() {
+                    @Override
+                    public void onAction(List<String> permissions) {
+                        startRecordDialog();
+                        voiceManager.setVoiceRecordListener(new VoiceManager.VoiceRecordCallBack() {
+                            @Override
+                            public void recDoing(long time, String strTime) {
+                                mRecordHintTv.setText(strTime);
+                            }
 
-            @Override
-            public void recVoiceGrade(int grade) {
-                voicLine.setVolume(grade);
-            }
+                            @Override
+                            public void recVoiceGrade(int grade) {
+                                voicLine.setVolume(grade);
+                            }
 
-            @Override
-            public void recStart(boolean init) {
-                mIvPauseContinue.setImageResource(R.mipmap.icon_pause);
-                voicLine.setContinue();
-            }
+                            @Override
+                            public void recStart(boolean init) {
+                                mIvPauseContinue.setImageResource(R.mipmap.icon_pause);
+                                voicLine.setContinue();
+                            }
 
-            @Override
-            public void recPause(String str) {
-                mIvPauseContinue.setImageResource(R.mipmap.icon_continue);
-                voicLine.setPause();
-            }
-            @Override
-            public void recFinish(long length, String strLength, String path) {
-                if (enRecordVoiceListener != null) {
-                    enRecordVoiceListener.onFinishRecord(length, strLength, path);
-                }
-            }
+                            @Override
+                            public void recPause(String str) {
+                                mIvPauseContinue.setImageResource(R.mipmap.icon_continue);
+                                voicLine.setPause();
+                            }
+                            @Override
+                            public void recFinish(long length, String strLength, String path) {
+                                if (enRecordVoiceListener != null) {
+                                    enRecordVoiceListener.onFinishRecord(length, strLength, path);
+                                }
+                            }
 
-            @Override
-            public void recCancel() {
+                            @Override
+                            public void recCancel() {
 
-            }
-        });
-        voiceManager.startVoiceRecord(mPath);
-
+                            }
+                        });
+                        voiceManager.startVoiceRecord(mPath);
+                    }
+                })
+                .onDenied(new Action<List<String>>() {
+                    @Override
+                    public void onAction(@NonNull List<String> permissions) {
+                        if (AndPermission.hasAlwaysDeniedPermission(mContext, permissions)) {
+                            Toast.makeText(mContext, "请开启录音权限", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).start();
     }
 
     /**
